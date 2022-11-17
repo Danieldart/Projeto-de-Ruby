5e0d457d9755a2e39851ccf0f7fc4a8213088b4a
Danieldart <daniel.veiga@grafeno.digital>
2022-11-17 14:28:35 -0300

    arquivo finalizado

--- Mudanças/Questão 2.rb
+++ Mudanças/Questão 2.rb
@@ -1,82 +1,53 @@
 require 'yaml'
 
-input = [
+inputs = [
 { "name": 'Maria Neusa de Aparecida', "cpf": '97905796671',
 "state": 'Sao Paulo', "value": '1234' },
 { "name": 'Ricardo Fontes', "cpf": '44010762900',
 "state": 'Rio Grande do Sul', "value": '567' }
 ]
-def outputs(output,itens,valor)
-    if paddings(valor["padding"]) == true && aligns(valor["align"]) == true
-        output <<"#{itens.ljust(valor["length"],"0")}"
-    elsif paddings(valor["padding"]) == true && aligns(valor["align"]) == false
-        output <<"#{itens.rjust(valor["length"],"0")}"
-    elsif paddings(valor["padding"]) == false && aligns(valor["align"]) == true
-        output <<"#{itens.ljust(valor["length"],"\s")}"
-    elsif paddings(valor["padding"]) == false && aligns(valor["align"]) == false
-        output <<"#{itens.rjust(valor["length"],"\s")}"
+FILE_NAME = 'format-1.yaml'
+def solution(inputs)
+    keys = YAML.load_file(FILE_NAME).keys
+    inputs.each do |input|
+      output = ""
+      keys.each do |key| 
+        output << format_word(input[key.to_sym], file_properties(key))
+      end
+      p output
     end
 end
-def aligns(value)
-    if value == 'left'
-        return true
-    else
-        return false
-    end
+
+def format_word(word, properties)
+  return transform_word(word, properties) if word_length(word, properties["length"])
+  format(word, properties["length"])
 end
-def paddings(value)
-    if value == 'zeroes'
-        return true
-    else
-        return false
-    end
+
+def format(word, length)
+  word[0, length]
 end
-def solucao(input)
-    for i in 0..1 do
-    output = ""
-    chaves = YAML.load_file('# format-2.yaml').keys
-    chaves.each do |chave|
-        valor = YAML.load_file('# format-2.yaml')[chave]
-        input[i].map do |itens|
-            itens = input[i]
-                if chave == "cpf" && itens[:cpf].size>=valor["length"]
-                    puts
-                    output << itens[:cpf][0,valor["length"]]
-                    break
-                elsif chave == "cpf"
-                    itens =itens[:cpf]
-                    output <<"#{itens.rjust(valor["length"],"0")}"
-                    break
-                end
-                if chave == "name" && itens[:name].size >=valor["length"]
-                    output << itens[:name][0,valor["length"]]
-                    break
-                elsif chave == "name"
-                    itens =itens[:name]
-                    outputs(output,itens,valor)
-                    break
-                end
-                if  chave == "value" && itens[:value].size >=valor["length"]
-                    output << itens[:value][0,valor["length"]]
-                    break
-                elsif chave == "value"
-                    itens =itens[:value]
-                    outputs(output,itens,valor)
-                    puts
-                    break
-                end
-                if  chave == "state" && itens[:state].size >=valor["length"]
-                    output <<itens[:state][0,valor["length"]]
-                    print output
-                    break
-                elsif chave == "state"
-                    itens =itens[:state]
-                    outputs(output,itens,valor)
-                    print output
-                    break
-                end
-    end
-        end
-    end
+
+def word_length(word, length)
+  word.size < length
 end
-solucao(input)
+
+def transform_word(word, properties)
+  types = {
+    right: word.rjust(properties["length"], character_types(properties["padding"])),
+    left: word.ljust(properties["length"], character_types(properties["padding"]))
+  }
+  types[properties["align"].to_sym] 
+end
+
+def character_types(type)
+  types = {
+    spaces: " ",
+    zeroes: "0"
+  }
+  types[type.to_sym]
+end
+
+def file_properties(key)
+  YAML.load_file(FILE_NAME)[key]
+end
+solution(inputs)
--- Mudanças/Questão 3.rb
+++ Mudanças/Questão 3.rb
@@ -1,32 +1,38 @@
 require "yaml"
+
 input = [
-    { "name": "Maria Neusa de Aparecida", "cpf": "97905796671",
-    "state": "Sao Paulo", "value": "1234" },
-    { "name": "Ricardo Fontes", "cpf": "44010762900",
-    "state": "Rio Grande do Sul", "value": "567" }
-    ]
+  { "name": "Maria Neusa de Aparecida", "cpf": "97905796671",
+  "state": "Sao Paulo", "value": "1234" },
+  { "name": "Ricardo Fontes", "cpf": "44010762900",
+  "state": "Rio Grande do Sul", "value": "567" }
+  ]
 
-def solucao(arg)
-    s =YAML.load_file('# format-1.yaml')
-    s.each do |chave, valor|
-        c =arg[0,11]
-        n =arg[11,14]
-        val =arg[25,15]
-        str1 = "cpf, name, value"
-        str2 = ("#{c} | #{n} | #{val}")
-        keys = str1.split(", ") 
-        values = str2.split("|") 
-        dictionary = {}
-        while dictionary.size < 3
-            for i in 0..2
-                dictionary[keys[i]] = values[i]
-        end
-            end
-        print(dictionary)
-        break
-
-        
-    end    
-    
+def solution(string)
+  cpf =string[0,11]
+  name =string[11,14]
+  value =string[25,15] 
+  format_string(cpf,name,value)
 end 
-solucao("97905796671Maria Neusa de00001234")
+
+def format_string(cpf,name,value)
+  str1 = "cpf, name, value"
+  str2 = ("#{cpf} | #{name} | #{value}")
+  keys = str1.split(", ") 
+  values = str2.split("|") 
+  create_hash(keys, values, str1)
+end
+
+def create_hash(keys, values, str1)
+  hash = {}
+  while hash.size < 3
+      for i in 0..2
+        if keys[i] == "value"
+          hash[keys[i]] = values[i].to_i
+        else
+          hash[keys[i]] = values[i] 
+        end
+  end
+      end
+      p hash
+end
+solution("44010762900Ricardo Fontes00000567")
